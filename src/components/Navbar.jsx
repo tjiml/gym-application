7e9d9189556753841117adcbd7811bb982ff2368
Jim Le <tjiml@Jims-MacBook-Pro.local>
2024-01-03 14:42:54 -0700

    navbar and paginate fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,39 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
 
-    const [toggler, setToggler] = useState(false);
+    const [click, setClick] = useState(false);
+    
+    function handleClick() {
+        setClick(!click);
+    }
 
-    function handleToggler() {
-        setToggler(!toggler);
+    function closeMobileMenu() {
+        setClick(false);
     }
 
     return (
         <>
-        <nav className="navbar navbar-expand-lg">
-            <div className="container-fluid">
-                <Link to="/" className="nav-logo"><span>TJL</span> FIT</Link>
-                    <button onClick={handleToggler} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className={toggler ? "bi bi-x" : "bi bi-list"}></span>
-                    </button>
 
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul>
-                    <li className="list-inline-item"><Link to="/" className="nav-link">Home</Link></li>
-                    <li className="list-inline-item"><Link to="/exercise-page" className="nav-link">Exercises</Link></li>
-                </ul>
-            </div>
+        <nav className="nav-container">
 
-            </div>
+        <div className="nav-logo">
+            <Link to="/" className="nav-logo"><span>TJL</span> FIT</Link>
+        </div>
+
+        <div className="menu-icon" onClick={handleClick}>
+            <i className="bi bi-list"></i>
+        </div>
+
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+
+            <li className="x-nav list-inline-item" onClick={closeMobileMenu}><i className="bi bi-x-lg"></i></li>
+            <li className="list-inline-item" onClick={closeMobileMenu}><Link to="/" >HOME</Link></li>
+            <li className="list-inline-item" onClick={closeMobileMenu}><Link to="/exercise-page">EXERCISES</Link></li>
+
+        </ul>
+
         </nav>
+
         </>
     )
 }
